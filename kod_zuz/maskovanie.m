81e3c405fddcf59dbb724c7d0c7b35c96d247e77
Anzu <62065484+cMonedula@users.noreply.github.com>
2025-08-07 16:39:17 +0200

    zakladny export maskovanych pngciek
    ono je to skor proof of concept, ale aj tak ma to tesi - teraz pridat normalizaciu a uvidime co dalej, mozno z toho pojde buildnut aj nejaky testovaci model?

--- kod_zuz/maskovanie.m
+++ kod_zuz/maskovanie.m
@@ -1,7 +1,41 @@
-clear all;
+clear all; close all;
 
-info=dicominfo("C:\Users\akara\Desktop\Martin_APVV\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm020.dcm");
+pocet=length(dir('..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\*.dcm'));
+
+info=dicominfo("..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm001.dcm");
 snimka=dicomread(info);
 
 figure
 imshow(snimka,[]);
+
+maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
+        'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
+        'LineWidth',1);
+
+maska=createMask(maska_elipsa);
+close;
+
+%% toto ich maskne, exportuje a normalizuje (TBA)
+for por=1:pocet
+    info=dicominfo(["..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm"+sprintf("%03d",por)+".dcm"]);
+    snimka=dicomread(info);
+
+%     figure
+%     imshow(snimka,[]);
+
+%     maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
+%         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
+%         'LineWidth',1);
+
+%     maska=createMask(maska_elipsa);
+%     imshow(maska)
+
+    snimka_masked=immultiply(snimka,maska);
+%     figure()
+    imshow(snimka_masked);
+    
+    nazov_suboru=sprintf('/test_export/test%d.png',por);
+    imwrite(cast(snimka,"uint16"),[pwd nazov_suboru],'Alpha',cast(maska,"double"))
+    fprintf('Snimka cislo %d z %d uspesne ulozena!\n',por,pocet);
+    close;
+end
