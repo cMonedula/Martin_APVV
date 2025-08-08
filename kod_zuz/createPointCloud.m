d3ff21ca960d3069f2575ded346530f5926ebe97
Anzu <62065484+cMonedula@users.noreply.github.com>
2025-08-08 10:31:41 +0200

    pokracovanie nacitavania suborov
    priebezny quicksave predtym nez tam vletim s dicomreadvolume a vsetko to prestane fungovat :skull:

--- kod_zuz/createPointCloud.m
+++ kod_zuz/createPointCloud.m
@@ -12,50 +12,57 @@ end
 
 status=mkdir('sourceData'); %vytvori priecinok kam idu data s ktorymi pracujeme, ak este neexistuje
 sourceContent=dir('sourceData'); %nacita si obsah priecinku so vstupnymi datami
-if status==0||(sum([sourceContent.isdir])-2)==0
+sourceContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu priecinkov (. a ..)
+if status==0||sum([sourceContent.isdir])==0
     fprintf('Skript sa ukonci kvoli chybajucim vstupnym datam.\nNaplnte vstupny priecinok sourceData DICOM snimkami v prislusnych podpriecinkoch a znovu spustite skript.\nPre pokracovanie stlacte akukolvek klavesu.\n');
     pause;
     return
 end
 clear status;
 
-%%
-pocet=length(dir('..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\*.dcm'));
-
-info=dicominfo("..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm001.dcm");
-snimka=dicomread(info);
-
-figure
-imshow(snimka,[]);
-
-maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
-        'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
-        'LineWidth',1);
-
-maska=createMask(maska_elipsa);
-close;
-
-%% toto ich maskne, exportuje a normalizuje (TBA)
-for por=1:pocet
-    info=dicominfo(["..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm"+sprintf("%03d",por)+".dcm"]);
-    snimka=dicomread(info);
-
-%     figure
-%     imshow(snimka,[]);
-
-%     maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
+%% v tejto casti kodu by sa vytvorila maska, ALE kedze budeme nacitavat data z roznych uhlov tak som sa rozhodla to radsej nechat na denoising podla confidence skore
+% pocet=length(dir('..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\*.dcm'));
+% 
+% info=dicominfo("..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm001.dcm");
+% snimka=dicomread(info);
+% 
+% figure
+% imshow(snimka,[]);
+% 
+% maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
 %         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
 %         'LineWidth',1);
+% 
+% maska=createMask(maska_elipsa);
+% close;
+%% toto si snimky nacita a umiestni (no masking involved)
+sourceCount=length(sourceContent); %spocita kolko priecinkov mame (kolko sad obrazkov ideme umiestnovat)
+for i=1:sourceCount
+    folderPath=[string(sourceContent(i).folder)+'\'+string(sourceContent(i).name)];
+    folderContent=dir([string(folderPath)+'\*.dcm']); %nacita si obsah priecinku so vstupnymi snimkami
+    folderContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu suborov (. a ..)
+    snimkaCount=length(folderContent); %zistime kolko toho mame v priecinku
+    for por=1:snimkaCount
+        info=dicominfo([folderPath+"\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm"+sprintf("%03d",por)+".dcm"]);
+        snimka=dicomread(info);
 
-%     maska=createMask(maska_elipsa);
-%     imshow(maska)
+    %     figure
+    %     imshow(snimka,[]);
 
-    snimka_masked=immultiply(snimka,maska);
-%     figure()
-    imshow(snimka_masked);
-    
-    nazov_suboru=sprintf('/test_export/test%d.png',por);
-    imwrite(cast(snimka,"uint16"),[pwd nazov_suboru],'Alpha',cast(maska,"double"))
-    fprintf('Snimka cislo %d z %d uspesne ulozena!\n',por,pocet);
-    close;
+    %     maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
+    %         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
+    %         'LineWidth',1);
+
+    %     maska=createMask(maska_elipsa);
+    %     imshow(maska)
+
+        snimka_masked=immultiply(snimka,maska);
+    %     figure()
+        imshow(snimka_masked);
+
+        nazov_suboru=sprintf('/test_export/test%d.png',por);
+        imwrite(cast(snimka,"uint16"),[pwd nazov_suboru],'Alpha',cast(maska,"double"))
+        fprintf('Snimka cislo %d z %d uspesne ulozena!\n',por,pocet);
+        close;
+    end
 end
