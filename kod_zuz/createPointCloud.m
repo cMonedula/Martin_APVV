397273c5d938c6c90116e8537dd6645bfd5693b6
Anzu <62065484+cMonedula@users.noreply.github.com>
2025-08-08 10:44:04 +0200

    prvotny volume
    OpenGL minus :kekw:

--- kod_zuz/createPointCloud.m
+++ kod_zuz/createPointCloud.m
@@ -38,31 +38,35 @@ clear status;
 %% toto si snimky nacita a umiestni (no masking involved)
 sourceCount=length(sourceContent); %spocita kolko priecinkov mame (kolko sad obrazkov ideme umiestnovat)
 for i=1:sourceCount
-    folderPath=[string(sourceContent(i).folder)+'\'+string(sourceContent(i).name)];
-    folderContent=dir([string(folderPath)+'\*.dcm']); %nacita si obsah priecinku so vstupnymi snimkami
-    folderContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu suborov (. a ..)
-    snimkaCount=length(folderContent); %zistime kolko toho mame v priecinku
-    for por=1:snimkaCount
-        info=dicominfo([folderPath+"\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm"+sprintf("%03d",por)+".dcm"]);
-        snimka=dicomread(info);
-
-    %     figure
-    %     imshow(snimka,[]);
-
-    %     maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
-    %         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
-    %         'LineWidth',1);
-
-    %     maska=createMask(maska_elipsa);
-    %     imshow(maska)
-
-        snimka_masked=immultiply(snimka,maska);
-    %     figure()
-        imshow(snimka_masked);
-
-        nazov_suboru=sprintf('/test_export/test%d.png',por);
-        imwrite(cast(snimka,"uint16"),[pwd nazov_suboru],'Alpha',cast(maska,"double"))
-        fprintf('Snimka cislo %d z %d uspesne ulozena!\n',por,pocet);
-        close;
-    end
+    folderPath(i)=[string(sourceContent(i).folder)+'\'+string(sourceContent(i).name)+'\'];
+%     folderContent=dir([string(folderPath)+'\*.dcm']); %nacita si obsah priecinku so vstupnymi snimkami
+%     folderContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu suborov (. a ..)
+%     snimkaCount=length(folderContent); %zistime kolko toho mame v priecinku
+    [V,spatial]=dicomreadVolume(folderPath(i));
+    squeeze(V);
+    vol=volshow(V);
 end
+
+% for por=1:snimkaCount
+%     info=dicominfo([folderPath+"\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm"+sprintf("%03d",por)+".dcm"]);
+%     snimka=dicomread(info);
+%     
+%     %     figure
+%     %     imshow(snimka,[]);
+%     
+%     %     maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
+%     %         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
+%     %         'LineWidth',1);
+%     
+%     %     maska=createMask(maska_elipsa);
+%     %     imshow(maska)
+%     
+%     snimka_masked=immultiply(snimka,maska);
+%     %     figure()
+%     imshow(snimka_masked);
+%     
+%     nazov_suboru=sprintf('/test_export/test%d.png',por);
+%     imwrite(cast(snimka,"uint16"),[pwd nazov_suboru],'Alpha',cast(maska,"double"))
+%     fprintf('Snimka cislo %d z %d uspesne ulozena!\n',por,pocet);
+%     close;
+% end
