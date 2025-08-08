e67f52675fe294704e1855b2e832afaed7b4df4b
Anzu <62065484+cMonedula@users.noreply.github.com>
2025-08-08 13:31:03 +0200

    mame prve vystupy!
    ok, su to len trapne zasumene kocky, ale :sparkles: slay :sparkles:

--- kod_zuz/createPointCloud.m
+++ kod_zuz/createPointCloud.m
@@ -10,6 +10,11 @@ while status==0
     status=mkdir('pointCloudData'); %vytvori priecinok na ukladanie dat ktore ziskame ako vystup z tohto skriptu
 end
 
+status=0;
+while status==0
+    status=mkdir('niiData'); %vytvori priecinok na ukladanie volume vystupov vo formate .nii
+end
+
 status=mkdir('sourceData'); %vytvori priecinok kam idu data s ktorymi pracujeme, ak este neexistuje
 sourceContent=dir('sourceData'); %nacita si obsah priecinku so vstupnymi datami
 sourceContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu priecinkov (. a ..)
@@ -20,63 +25,44 @@ if status==0||sum([sourceContent.isdir])==0
 end
 clear status;
 
-%% v tejto casti kodu by sa vytvorila maska, ALE kedze budeme nacitavat data z roznych uhlov tak som sa rozhodla to radsej nechat na denoising podla confidence skore
-% pocet=length(dir('..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\*.dcm'));
-% 
-% info=dicominfo("..\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm001.dcm");
-% snimka=dicomread(info);
-% 
-% figure
-% imshow(snimka,[]);
-% 
-% maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
-%         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
-%         'LineWidth',1);
-% 
-% maska=createMask(maska_elipsa);
-% close;
-%% toto si snimky nacita a umiestni (no masking involved)
+%% toto si snimky nacita, umiestni a vytvori volume subor
 sourceCount=length(sourceContent); %spocita kolko priecinkov mame (kolko sad obrazkov ideme umiestnovat)
+
 for i=1:sourceCount
-    folderPath(i)=[string(sourceContent(i).folder)+'\'+string(sourceContent(i).name)+'\'];
+    folderPath=string(sourceContent(i).folder);
+    outputName(i)=string(sourceContent(i).name);
+end
+%     folderPath(i)=[string(sourceContent(i).folder)+'\'+string(sourceContent(i).name)+'\'];
 %     folderContent=dir([string(folderPath)+'\*.dcm']); %nacita si obsah priecinku so vstupnymi snimkami
 %     folderContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu suborov (. a ..)
 %     snimkaCount=length(folderContent); %zistime kolko toho mame v priecinku
-    sourceTable(i) = dicomCollection(folderPath(i));
-    
-    V(i) = dicomreadVolume(sourceTable(i),"s1",MakeIsotropic=true);
-    V(i) = squeeze(V(i));
+sourceTable = dicomCollection(folderPath,"IncludeSubfolders",true);
 
-    intensity = [0 20 40 120 220 1024];
-    alpha = [0 0 0.15 0.3 0.38 0.5];
-    color = ([0 0 0; 43 0 0; 103 37 20; 199 155 97; 216 213 201; 255 255 255])/255;
-    queryPoints = linspace(min(intensity),max(intensity),256);
-    amap = interp1(intensity,alpha,queryPoints)';
-    cmap = interp1(intensity,color,queryPoints);
-
-    vol=volshow(V(i),Colormap=cmap,Alphamap=amap);
+for j=1:sourceCount
+sourceTable.Filenames{j,1}(1)=[]; %znizime antialiasing eliminaciou prvej a poslednej snimky
+sourceTable.Filenames{j,1}(sourceTable.Frames(j)-1)=[]; %znizime antialiasing eliminaciou prvej a poslednej snimky
+sourceTable.Frames(j)=sourceTable.Frames(j)-2; %odoberieme z poctu snimok tie dve
 end
 
-% for por=1:snimkaCount
-%     info=dicominfo([folderPath+"\Data_Dano_poslat\P1_control\3D\P1_T1_FLASH_3D\MRIm"+sprintf("%03d",por)+".dcm"]);
-%     snimka=dicomread(info);
-%     
-%     %     figure
-%     %     imshow(snimka,[]);
-%     
-%     %     maska_elipsa=drawellipse('SemiAxes',[98.2730073590999 101.126692004371],...
-%     %         'Center',[135.695774647887 142.185915492958],'RotationAngle',9.79775988274304,'Color',[0.0588235294117647 1 1],...
-%     %         'LineWidth',1);
-%     
-%     %     maska=createMask(maska_elipsa);
-%     %     imshow(maska)
-%     
-%     snimka_masked=immultiply(snimka,maska);
-%     %     figure()
-%     imshow(snimka_masked);
-%     
+for i=1:sourceCount
+    V=dicomreadVolume(sourceTable,char(sourceTable.Properties.RowNames(i)),"MakeIsotropic",true);
+    V=squeeze(V);
+    niftiwrite(V,string(["niiData\"+outputName(i)+".nii"]));
+    clear V;
+end
+
+intensity = [0 20 40 120 220 1024];
+alpha = [0 0 0.15 0.3 0.38 0.5];
+color = ([0 0 0; 43 0 0; 103 37 20; 199 155 97; 216 213 201; 255 255 255])/255;
+queryPoints = linspace(min(intensity),max(intensity),256);
+amap = interp1(intensity,alpha,queryPoints)';
+cmap = interp1(intensity,color,queryPoints);
+
+for i=1:sourceCount
+    V=niftiread(string(["niiData\"+outputName(i)+".nii"]));
+    vol=volshow(V,"Colormap",cmap,"Alphamap",amap);
+end
 %     nazov_suboru=sprintf('/test_export/test%d.png',por);
 %     imwrite(cast(snimka,"uint16"),[pwd nazov_suboru],'Alpha',cast(maska,"double"))
 %     fprintf('Snimka cislo %d z %d uspesne ulozena!\n',por,pocet);
 %     close;
-% end
