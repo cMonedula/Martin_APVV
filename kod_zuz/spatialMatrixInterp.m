48cd562a782213124e10caa4ce11e9ec084ae7b1
Anzu <62065484+cMonedula@users.noreply.github.com>
2025-08-12 11:19:01 +0200

    rychla zaloha
    predtym nez kompletne prerobim nacitavanie suborov (dicomCollection je kravina)

--- kod_zuz/spatialMatrixInterp.m
+++ kod_zuz/spatialMatrixInterp.m
@@ -1,14 +1,15 @@
-function [interpVolume] = spatialMatrixInterp(axial,coronal,sagital,cor_address,sag_address,needsRegistration)
+function [interpVolume] = spatialMatrixInterp(axial,coronal,sagittal,cor_address,sag_address,needsRegistration)
 %SPATIALMATRIXINTERP tato funkcia obsahuje interpolacnu metodu pre
 %trojrozmerne matice
 %   Detailed explanation goes here
-%% registracia koronalneho a sagitalneho rezu (trva dlho! obcas az ~25 min)
+%% registracia koronalneho a sagittalneho rezu (trva dlho! obcas az ~25 min)
 if needsRegistration==true
     [optimizer,metric]=imregconfig('monomodal');
-    coronal_reg=imregister(coronal,axial,'affine',optimizer,metric);
-    sagital_reg=imregister(sagital,axial,'affine',optimizer,metric);
+    optimizer.MaximumStepLength=0.00625;
+    coronal_reg=imregister(coronal,axial,'affine',optimizer,metric,'DisplayOptimization',true);
+    sagittal_reg=imregister(sagittal,axial,'affine',optimizer,metric,'DisplayOptimization',true);
     niftiwrite(coronal_reg,cor_address);
-    niftiwrite(sagital_reg,sag_address);
+    niftiwrite(sagittal_reg,sag_address);
 end
 
 %% vytvorenie interpolacnej matice
@@ -16,21 +17,21 @@ res=0.5; %interpolacne rozlisenie - 1 je bez zmeny, >1 je zmensenie, <1 je zvacs
 
 [x_ax,y_ax,z_ax]=meshgrid(1:size(axial,1),1:size(axial,2),1:size(axial,3));
 [x_cor,y_cor,z_cor]=meshgrid(1:size(coronal,1),1:size(coronal,2),1:size(coronal,3));
-[x_sag,y_sag,z_sag]=meshgrid(1:size(sagital,1),1:size(sagital,2),1:size(sagital,3));
+[x_sag,y_sag,z_sag]=meshgrid(1:size(sagittal,1),1:size(sagittal,2),1:size(sagittal,3));
 
 %urcenie najvacsich rozmerov v pripade ze su snimky alebo sady snimok nejak
 %odlisne
-max_x=max([size(axial,1),size(coronal,1),size(sagital,1)]);
-max_y=max([size(axial,2),size(coronal,2),size(sagital,2)]);
-max_z=max([size(axial,3),size(coronal,3),size(sagital,3)]);
+max_x=max([size(axial,1),size(coronal,1),size(sagittal,1)]);
+max_y=max([size(axial,2),size(coronal,2),size(sagittal,2)]);
+max_z=max([size(axial,3),size(coronal,3),size(sagittal,3)]);
 
 [x,y,z]=meshgrid(1:res:max_x,1:res:max_y,1:res:max_z);
 
 %%samotna interpolacia
 interp_axial=interp3(x_ax,y_ax,z_ax,cast(axial,'double'),x,y,z,'makima');
 interp_coronal=interp3(x_cor,y_cor,z_cor,cast(coronal,'double'),x,y,z,'makima');
-interp_sagital=interp3(x_sag,y_sag,z_sag,cast(sagital,'double'),x,y,z,'makima');
+interp_sagittal=interp3(x_sag,y_sag,z_sag,cast(sagittal,'double'),x,y,z,'makima');
 
-interpVolume=(interp_axial+interp_coronal+interp_sagital)/3;
+interpVolume=(interp_axial+interp_coronal+interp_sagittal)/3;
 end
 
