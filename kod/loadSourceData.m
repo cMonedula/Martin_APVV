aef0b0ac22eca5019efd9c5190b4d8cb445f9b69
Anzu <62065484+cMonedula@users.noreply.github.com>
2026-07-06 16:34:29 +0200

    upratovanie

--- kod/loadSourceData.m
+++ kod/loadSourceData.m
@@ -19,53 +19,55 @@ end
 
 sourceContent = []; % podpriecinky sourceData
 sourcePath = ''; % adresa odkazujuca na sourceData 
-% inputName = [];
+inputName = [];
 volumeContent = []; % ulozene rekonstruovane modely v savedData
 volumePath = ''; % adresa na savedData
-% volumeName = [];
+volumeName = [];
 
 %% uvodny check priecinkov na data
 
-status=0;
-while status==0
-    status=mkdir('savedData'); %vytvori priecinok na ukladanie volume vystupov vo formate .nii
+status = 0;
+while status == 0
+    status = mkdir('savedData'); %vytvori priecinok na ukladanie volume vystupov vo formate .nii
 end
 
-status=mkdir('sourceData'); %vytvori priecinok kam idu data s ktorymi pracujeme, ak este neexistuje
-sourceContent=dir('sourceData'); %nacita si obsah priecinku so vstupnymi datami
-sourceContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu priecinkov (. a ..)
+status = mkdir('sourceData'); %vytvori priecinok kam idu data s ktorymi pracujeme, ak este neexistuje
+sourceContent = dir('sourceData'); %nacita si obsah priecinku so vstupnymi datami
+sourceContent(1:2) = []; %vymaze prve dva zbytocne prvky zo zoznamu priecinkov (. a ..)
 
-if status==0||sum([sourceContent.isdir])==0
+if status == 0 || sum([sourceContent.isdir]) == 0
     logMsg("Vstupné dáta neboli úspešne načítané. Naplňte vstupný priečinok sourceData DICOM snímkami rozdelenými do podpriečinkov a znovu spustite ich načítavanie.");
     return;
 end
 
 clear status; logMsg("Vstupné dáta úspešne načítané.");
 
-sourceCount=length(sourceContent); %spocita kolko priecinkov mame (kolko sad obrazkov mame k dispozicii)
+sourceCount = length(sourceContent); %spocita kolko priecinkov mame (kolko sad obrazkov mame k dispozicii)
 
-inputName = strings(1, sourceCount); 
-
-for i=1:sourceCount
-    sourcePath=string(sourceContent(i).folder);
-    inputName(i)=string(sourceContent(i).name);
-end
-
-volumeContent=dir('savedData'); %nacita si obsah priecinku so spracovanymi volume datami
-volumeContent(1:2)=[]; %vymaze prve dva zbytocne prvky zo zoznamu priecinkov (. a ..)
-volumeCount=length(volumeContent);
-
-volumeName = strings(1, sourceCount); 
-
-if volumeCount~=0
-    for i=1:volumeCount
-        volumePath=string(volumeContent(i).folder);
-        volumeName(i)=string(volumeContent(i).name);
-        volumeName(i)=erase(volumeName(i),".nii");
+if sourceCount > 0
+    inputName = strings(1, sourceCount); 
+    
+    for i = 1:sourceCount
+        sourcePath = string(sourceContent(i).folder);
+        inputName(i) = string(sourceContent(i).name);
     end
 end
 
-logMsg("Uložené modely boli úspešne načítané.")
+volumeContent = dir('savedData'); %nacita si obsah priecinku so spracovanymi volume datami
+volumeContent(1:2) = []; %vymaze prve dva zbytocne prvky zo zoznamu priecinkov (. a ..)
+volumeCount = length(volumeContent);
+
+if volumeCount > 0
+    volumeName = strings(1, sourceCount); 
+
+    for i = 1:volumeCount
+        volumePath = string(volumeContent(i).folder);
+        volumeName(i) = string(volumeContent(i).name);
+        volumeName(i) = erase(volumeName(i),".nii");
+    end
+
+    logMsg("Uložené modely boli úspešne načítané.")
+end
 
 logMsg("Dáta pripravené.")
 end
